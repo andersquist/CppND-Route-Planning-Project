6e09f982231b365d3ce062f850ec91c50808f9ec
Anders Quist <anders@qinfo.se>
2019-05-05 13:29:51 +0200

    9. CODE: Populate the route node vector.

--- src/route_model.cpp
+++ src/route_model.cpp
@@ -2,5 +2,14 @@
 #include <iostream>
 
 RouteModel::RouteModel(const std::vector<std::byte> &xml) : Model(xml) {
+    for (int i = 0; i < this->Nodes().size(); i++ ) {
+        auto current = this->Nodes()[i];
 
+        // Create node on stack
+        RouteModel::Node routeNode (i, this, current);
+
+        // Copy it to the vector
+        m_Nodes.push_back(routeNode);
+
+    }
 }
